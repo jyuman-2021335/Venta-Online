4953f6be80573161466106fdc8507f2cad45e9a1
Jefferson Yuman Pérez <106627899+jyuman-2021335@users.noreply.github.com>
2023-03-08 00:16:48 -0600

    Registro Usuarios, CRUD Categorias, Peticiones de Postman

---
+++ controllers/categoria.js
@@ -0,0 +1,95 @@
+//Importacion
+const { response, request } = require('express');
+const Categoria = require('../models/categoria');
+
+const obtenerCategorias = async(req = request, res = response) => {
+    //Condición, me busca solo las categorias que tengan estado en true
+    const query = { estado: true };
+
+    const listaCategorias = await Promise.all([
+        Categoria.countDocuments(query),
+        Categoria.find(query).populate('usuario', 'nombre')
+    ]);
+
+    res.json({
+        msg: 'GET API de usuarios',
+        listaCategorias
+    });
+}
+
+const obtenerCategoriaPorId = async(req = request, res = response) => {
+
+    const { id } = req.params;
+    const categoria = await Categoria.findById(id).populate('usuario', 'nombre');
+
+    res.json({
+        msg: 'Categoria por Id',
+        categoria
+    });
+}
+
+const crearCategoria = async (req = request, res = response) => {
+
+    const nombre = req.body.nombre.toUpperCase();
+
+    //Validación para encontrar una categoria por nombre en la DB
+    const categoriaDB = await Categoria.findOne({ nombre });
+    if (categoriaDB) {
+        return res.status(400).json({
+            msg: `La categoria  ${categoriaDB.nombre}, ya existe en la DB`
+        });
+    }
+
+    //Generar la data(información a almacenar en la base de datos) a guardar
+    const data = {
+        nombre,
+        usuario: req.usuario._id
+    }
+
+    const categoriaAgregada = new Categoria(data);
+
+    //Guardar en la base de datos
+    await categoriaAgregada.save();
+
+    res.status(201).json({
+        msg: 'Post de Categoria',
+        categoriaAgregada
+    })
+}
+
+const actualizarCategoria = async(req = request, res = response) => {
+
+    const { id } = req.params;
+    const {_id, estado, usuario, ...data} = req.body; // el ... es el operador de express
+
+    data.nombre = data.nombre.toUpperCase();     //Cambiamos todo a mayúsculas
+    data.usuario = req.usuario._id; //Hacemos referencia al  usuario que hizo el put por medio del token
+
+    //Edición de categoría
+    const categoria = await Categoria.findByIdAndUpdate(id, data, {new: true}); //new: true  Sirve para enviar el nuevo documento actualizado
+
+    res.json({
+        msg: 'PUT de Categoria',
+        categoria
+    });
+}
+
+const borrarCategoria = async(req = request, res = response) => {
+
+    const { id } = req.params;
+    const categoriaBorrada = await Categoria.findByIdAndUpdate(id, { estado: false }, { new: true });
+
+    res.json({
+        msg: 'Borrar Categoria',
+        id
+    });
+}
+
+
+module.exports = {
+    obtenerCategorias,
+    obtenerCategoriaPorId,
+    crearCategoria,
+    actualizarCategoria,
+    borrarCategoria
+}
---
+++ routes/categoria.js
@@ -0,0 +1,42 @@
+//Importaciones
+const { Router } = require('express');
+const { check } = require('express-validator');
+const { login } = require('../controllers/auth');
+const { obtenerCategorias, obtenerCategoriaPorId, crearCategoria, actualizarCategoria, borrarCategoria } = require('../controllers/categoria');
+const { existeCategoriaPorId } = require('../helpers/db-validators');
+const { validarCampos } = require('../middlewares/validar-campos');
+const { validarJWT } = require('../middlewares/validar-jwt');
+
+const router = Router();
+
+// Obtener todas las categorias - público
+router.get('/', obtenerCategorias);
+
+// Obtener una categoria por el id - público
+router.get('/:id', [
+    check('id', 'No es un id de mongo DB válido').isMongoId(),
+    check('id').custom(existeCategoriaPorId),
+    validarCampos
+], obtenerCategoriaPorId);
+
+// Crear categoria - privado - cualquier persona con un token valido puede realizar dicha funcion
+router.post('/agregar', [
+    validarJWT,
+    check('nombre', 'El nombre  de la categoria es obligatorio').not().isEmpty(),
+    validarCampos
+], crearCategoria);
+
+// Actualizar categorias - privado - se requiere id y un token valido
+router.put('/editar/:id', [
+    validarJWT,
+    check('id', 'No es un id de mongo DB válido').isMongoId(),
+    check('id').custom(existeCategoriaPorId),
+    check('nombre', 'El nombre  de la categoria es obligatorio').not().isEmpty(),
+    validarCampos
+], actualizarCategoria);
+
+// Borrar una categoria - privado - se requiere id y un token valido
+router.delete('/eliminar/:id', borrarCategoria);
+
+
+module.exports = router;
